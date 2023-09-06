1eeb73986839d0d6ea89524b86964f83f0084b3a
Manoj naik <chittinaik@gamil.com>
2023-09-06 17:35:27 +0530

    [Manoj] add:FibonacciSeries

---
+++ LogicalProblems/LogicalProblems/Fibonacci Series.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class Fibonacci_Series
+    {
+        public static void  FS()
+        {
+
+            Console.WriteLine("Enter The Number: ");
+            int m=int.Parse(Console.ReadLine());
+
+            int f = 0, s = 1;
+            
+               
+              for(int i=1; i < m; i++)
+                {
+                    Console.WriteLine(f);
+                int next = f + s;
+                f = s;
+                s = next;
+                    
+
+                }
+            
+        }
+    }
+}
---
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Problem Number: " + "1. Fibonacci Series");
+            int n=int.Parse(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1:
+
+                    Fibonacci_Series.FS();
+                    Console.ReadLine();
+                    break;
+
+
+            }
+        }
+    }
+}
